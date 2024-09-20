42d513eb5e33548fa9186e3eec3cbccc2534d7e9
Simona Katarina Hlinka <simonakatarinah@gmail.com>
2024-09-20 23:17:24 +0200

    Creating a new weather project with react
    Created and empty file and then using the terminal to create the react app.
    
    Using the terminal imported necessary NPM libraries that includes React and Axios.
    
    Changed the name for App.js to WeatherSearch.js to match the project that I am transferring from Code Sandbox into this VS code folder/files.
    
    Included all the code from the original Code Sandbox project we initially created.
    Index.js code added and WeatherSearch.js code added.
    
    Deleted unnecessary  logo images and icon image.
    
    Changed all the App.js into WeatherSearch.js that needed to be matched for our code to run properly without syntax errors.
    
    Tested the localhost to see if everything was running smoothly and had no warnings/errors.
    
    Final edited folder/files now publishing to GitHub and hosting it on Netlify.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import WeatherSearch from './WeatherSearch';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<WeatherSearch />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './WeatherSearch';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import WeatherSearch from "./WeatherSearch";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./App.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <StrictMode>
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherSearch />
+    </div>
+  </StrictMode>
+)
